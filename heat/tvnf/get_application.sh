45612282592aa13cb66313d2769eddf38c7ad6b3
You Victor <victor.you@nokia-sbell.com>
2018-09-28 15:50:28 +0800

    add dns

--- heat/tvnf/get_application.sh
+++ heat/tvnf/get_application.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Wait until Docker service is running
 
+# Wait until Docker service is running
 I=0
 while [[ $(systemctl is-active docker) != "active" ]]; do
   if [[ I -gt 10 ]]; then
@@ -10,9 +10,21 @@ while [[ $(systemctl is-active docker) != "active" ]]; do
   sleep 5
 done
 
-#docker load -i /root/neveexec_master.tar.gz
+# wait until network connection is OK
+ARTIFACTORY_IP=$(echo $1 | cut -d: -f1)
+K=0
+until ping "$ARTIFACTORY_IP" -c 1 || [[ K -gt 200 ]] ; do  
+  echo "waiting for network connection for the $K time"
+  sleep 1
+  K=$((K+1))
+done
 
-systemctl status network
+if [[ K -gt 200 ]]; then
+  echo "[DEBUG:] having waited for network connection for 200 seconds, exiting"
+  exit -1
+fi
+
+# download docker image and load
 DOCKER_URL="https://"$1"/artifactory/"$3"/"$2"/neveexec_master."$2".tar.gz"
 echo "[DEBUG:] Download Docker image from: "$DOCKER_URL
 
@@ -30,13 +42,29 @@ do
     sleep 5
 done
 echo "[DEBUG:] neveexec_master.tar.gz downloaded"
-#
-#IMAGE=$(docker load -i /tmp/devopsserver-mock-app.tar.gz --quiet | cut -d " " -f3)
-#
-## Run container from loaded image
-#docker run -it --network=host --name "devopsserver-mock-app" -d "$IMAGE"
-#if [[ $? -eq 0 ]]; then
-#  echo "MockApp version changed to $IMAGE successfully!"
-#else
-#  echo "Could not start container."
-#fi
+if docker load -i /tmp/neveexec_master.tar.gz --quiet ; then
+  echo "image loaded"
+else
+  echo "failed to load image"
+fi  
+
+# download test case zip and unzip
+TA_URL="https://"$1"/artifactory/"$3"/"$2"/NeVe_TA_RF."$2".zip"
+echo "[DEBUG:] Download TA cases from: "$TA_URL
+
+J=0
+while [ ! -f "/tmp/NeVe_TA_RF.zip" ]
+do
+    if [[ J -gt 5 ]]; then
+        echo "[DEBUG:] tried 5 times, exiting"
+        exit -1
+    fi
+    # Get Docker image from artifactory
+    echo "[DEBUG:] try for the $J time"
+    curl -X GET -f --retry 3 --retry-delay 2 -o "/tmp/NeVe_TA_RF.zip" "$TA_URL" -k
+    J=$((J+1))
+    sleep 5
+done
+echo "[DEBUG:] TA downloaded"
+sudo -u centos unzip -d /home/centos /tmp/NeVe_TA_RF.zip
+echo "[DEBUG:] TA extracted"
