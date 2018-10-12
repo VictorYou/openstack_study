62aa19393bdb02aede59c851154a6767a2aa00de
You Victor <victor.you@nokia-sbell.com>
2018-10-12 15:41:31 +0800

    user waitsignal to ensure init

--- heat/mystack/server_boot.sh
+++ heat/mystack/server_boot.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 echo "i am the booting script"
+echo "hello $NAME"
 touch /home/centos/created_by_boot_script
+
+K=0
+until [[ K -gt 100 ]]; do
+  sleep 1
+  echo "having waited for $K seconds"
+  K=$((K+1))
+done
+
+wc_notify --data-binary '{"status": "SUCCESS", "id": "id1", "data": "data1"}'
