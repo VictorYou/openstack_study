9e11c1eb45a7c091ba1b7df20b5e4cb10121f5ea
You Victor <victor.you@nokia-sbell.com>
2018-10-23 15:39:37 +0800

    use functions

--- heat/tvnf/server_init.sh
+++ heat/tvnf/server_init.sh
@@ -12,7 +12,6 @@ while [[ $(systemctl is-active docker) != "active" ]]; do
 done
 
 # wait until network connection is OK
-ARTIFACTORY_IP=$(echo $1 | cut -d: -f1)
 K=0
 until ping "$ARTIFACTORY_IP" -c 1 || [[ K -gt 200 ]] ; do  
   echo "waiting for network connection for the $K time"
@@ -25,70 +24,75 @@ if [[ K -gt 200 ]]; then
   exit -1
 fi
 
-# download docker image and load
-DOCKER_URL="https://"$ARTIFACTORY_IP":"$ARTIFACTORY_PORT"/artifactory/"$REPOSITORY_NAME"/"$APP_VERSION"/neveexec_master."$APP_VERSION".tar.gz"
-echo "[DEBUG:] Download Docker image from: "$DOCKER_URL
+function check_artifact_suffix()
+{
+  local artifactory_ip=$1
+  local artifactory_port=$2
+  local repository_name=$3
+  local app_version=$4
+  local artifact_name=$5
 
-J=0
-while [ ! -f "/tmp/neveexec_master.tar.gz" ]
-do
-    if [[ J -gt 5 ]]; then
-        echo "[DEBUG:] tried 5 times, exiting"
-        exit -1
-    fi
-    # Get Docker image from artifactory
-    echo "[DEBUG:] try for the $J time"
-    curl -X GET -f --retry 3 --retry-delay 2 -o "/tmp/neveexec_master.tar.gz" "$DOCKER_URL" -k
-    J=$((J+1))
-    sleep 5
-done
-echo "[DEBUG:] neveexec_master.tar.gz downloaded"
-if docker load -i /tmp/neveexec_master.tar.gz --quiet ; then
+  artifact=$(curl -uadmin:APidn22umAgCGbtZUDwYib5PcfrE1Rv1m9gXhwujZx3u4qYk -X GET "https://$artifactory_ip:$artifactory_port/artifactory/api/storage/$repository_name/$app_version" -k | jq -r '.children[]' | jq -r '.uri' | grep $artifact_name)
+
+  if [ -z $artifact ]; then
+    echo "artifact $artifact_name not found, exiting"
+    exit -1
+  fi
+
+  echo ${artifact#/$artifact_name}
+}
+
+function download_file()
+{
+  local artifactory_ip=$1
+  local artifactory_port=$2
+  local repository_name=$3
+  local app_version=$4
+  local artifact=$5
+  local time=$6
+
+  URL="https://"$artifactory_ip":"$artifactory_port"/artifactory/"$repository_name"/"$app_version"/"$artifact
+  echo "[DEBUG:] Download $artifact from: "$URL
+  
+  J=0
+  while [ ! -f "/tmp/$artifact" ]
+  do
+      if [[ J -gt $time ]]; then
+          echo "[DEBUG:] tried $time times, exiting"
+          exit -1
+      fi
+      echo "[DEBUG:] try for the $J time"
+      curl -X GET -f --retry 3 --retry-delay 2 -o "/tmp/$artifact" "$URL" -k
+      J=$((J+1))
+      sleep 5
+  done
+  echo "[DEBUG:] $artifact downloaded"
+}
+
+# download docker image and load
+SUFFIX=$(check_artifact_suffix "$ARTIFACTORY_IP" "$ARTIFACTORY_PORT" "$REPOSITORY_NAME" "$APP_VERSION" "neveexec_master")
+download_file "$ARTIFACTORY_IP" "$ARTIFACTORY_PORT" "$REPOSITORY_NAME" "$APP_VERSION" "neveexec_master"$SUFFIX 5
+
+if docker load -i /tmp/"neveexec_master"$SUFFIX --quiet ; then
   echo "image loaded"
 else
-  echo "failed to load image"
+  echo "failed to load image, exiting"
+  exit =1
 fi  
 
 # download test case zip and unzip
-TA_URL="https://"$ARTIFACTORY_IP":"$ARTIFACTORY_PORT"/artifactory/"$REPOSITORY_NAME"/"$APP_VERSION"/NeVe_TA_RF."$APP_VERSION".zip"
-echo "[DEBUG:] Download TA cases from: "$TA_URL
-
-J=0
-while [ ! -f "/tmp/NeVe_TA_RF.zip" ]
-do
-    if [[ J -gt 5 ]]; then
-        echo "[DEBUG:] tried 5 times, exiting"
-        exit -1
-    fi
-    # Get Docker image from artifactory
-    echo "[DEBUG:] try for the $J time"
-    curl -X GET -f --retry 3 --retry-delay 2 -o "/tmp/NeVe_TA_RF.zip" "$TA_URL" -k
-    J=$((J+1))
-    sleep 5
-done
-echo "[DEBUG:] TA downloaded"
-sudo -u centos unzip -d /home/centos /tmp/NeVe_TA_RF.zip
+SUFFIX=$(check_artifact_suffix "$ARTIFACTORY_IP" "$ARTIFACTORY_PORT" "$REPOSITORY_NAME" "$APP_VERSION" "NeVe_TA_RF")
+download_file "$ARTIFACTORY_IP" "$ARTIFACTORY_PORT" "$REPOSITORY_NAME" "$APP_VERSION" "NeVe_TA_RF"$SUFFIX 20
+sudo -u centos unzip -d /home/centos /tmp/"NeVe_TA_RF"$SUFFIX
 echo "[DEBUG:] TA extracted"
 
 # download jenkins configuration and jenkins jobs
-JENKINS_CONF_URL="https://"$ARTIFACTORY_IP":"$ARTIFACTORY_PORT"/artifactory/"$REPOSITORY_NAME"/"$APP_VERSION"/config."$APP_VERSION".xml"
-echo "[DEBUG:] Download jenkins configuration from: "$JENKINS_CONF_URL
+SUFFIX=$(check_artifact_suffix "$ARTIFACTORY_IP" "$ARTIFACTORY_PORT" "$REPOSITORY_NAME" "$APP_VERSION" "config")
+download_file "$ARTIFACTORY_IP" "$ARTIFACTORY_PORT" "$REPOSITORY_NAME" "$APP_VERSION" "config"$SUFFIX 20
 
-J=0
-while [ ! -f "/tmp/config.xml" ]
-do
-    if [[ J -gt 5 ]]; then
-        echo "[DEBUG:] tried 5 times, exiting"
-        exit -1
-    fi
-    echo "[DEBUG:] try for the $J time"
-    curl -X GET -f --retry 3 --retry-delay 2 -o "/tmp/config.xml" "$JENKINS_CONF_URL" -k
-    J=$((J+1))
-    sleep 5
-done
-echo "[DEBUG:] config.xml downloaded"
 echo "[DEBUG:] take config.xml and restart jenkins"
-cp /tmp/config.xml /var/lib/jenkins/config.xml
+
+cp /tmp/"config"$SUFFIX /var/lib/jenkins/config.xml
 chown jenkins:jenkins /var/lib/jenkins/config.xml
 systemctl restart jenkins
 
@@ -102,22 +106,8 @@ while [[ $(systemctl is-active jenkins) != "active" ]]; do
   sleep 5
 done
 
-JENKINS_JOB_CONF_URL="https://"$ARTIFACTORY_IP":"$ARTIFACTORY_PORT"/artifactory/"$REPOSITORY_NAME"/"$APP_VERSION"/run_fast_pass_ta."$APP_VERSION".xml"
-echo "[DEBUG:] Download jenkins job configuration from: "$JENKINS_JOB_CONF_URL
-
-J=0
-while [ ! -f "/tmp/run_fast_pass_ta.xml" ]
-do
-    if [[ J -gt 5 ]]; then
-        echo "[DEBUG:] tried 5 times, exiting"
-        exit -1
-    fi
-    echo "[DEBUG:] try for the $J time"
-    curl -X GET -f --retry 3 --retry-delay 2 -o "/tmp/run_fast_pass_ta.xml" "$JENKINS_JOB_CONF_URL" -k
-    J=$((J+1))
-    sleep 5
-done
-echo "[DEBUG:] run_fast_pass_ta.xml downloaded"
+SUFFIX=$(check_artifact_suffix "$ARTIFACTORY_IP" "$ARTIFACTORY_PORT" "$REPOSITORY_NAME" "$APP_VERSION" "run_fast_pass_ta")
+download_file "$ARTIFACTORY_IP" "$ARTIFACTORY_PORT" "$REPOSITORY_NAME" "$APP_VERSION" "run_fast_pass_ta"$SUFFIX 20
 
 J=0
 echo "[DEBUG:] download jenkins-cli.jar"
@@ -132,10 +122,11 @@ while ! wget http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar -O /root/jenkins-cli
 done
 echo "[DEBUG:] jenkins-cli.jar downloaded"
 
-if ! java -jar /root/jenkins-cli.jar -s http://127.0.0.1:8080/ -auth 'admin:123456' create-job run_fast_pass_ta < /tmp/run_fast_pass_ta.xml ; then
+if ! java -jar /root/jenkins-cli.jar -s http://127.0.0.1:8080/ -auth 'admin:123456' create-job run_fast_pass_ta < /tmp/"run_fast_pass_ta"$SUFFIX ; then
   echo "[DEBUG:] jenkins job not created, exiting"
   exit -1
 fi
 echo "[DEBUG:] jenkins job created"
 
 wc_notify -k --data-binary '{"status": "SUCCESS", "id": "id1", "data": "boot script finished"}'
+
